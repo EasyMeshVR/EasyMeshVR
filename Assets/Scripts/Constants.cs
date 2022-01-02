9e2069001c5defb74f9193e855c2d1bd94bd5525
Rami <44623786+Rami-J@users.noreply.github.com>
2022-01-01 22:13:37 -0500

    Make Constants class static

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace EasyMeshVR.Core
 {
-    public class Constants : MonoBehaviour
+    public static class Constants
     {
         public const byte MAX_PLAYERS_PER_ROOM = 4;
         public const string NETWORK_PLAYER_PREFAB_NAME = "Network Player";
