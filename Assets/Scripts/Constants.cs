d5473b4bd16cec8a3b21fd41865717a26fb3f742
Rami <44623786+Rami-J@users.noreply.github.com>
2022-01-01 21:52:38 -0500

    Add multiple player spawn points and position synchronization

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasyMeshVR.Core
+{
+    public class Constants : MonoBehaviour
+    {
+        public static byte MAX_PLAYERS_PER_ROOM = 4;
+        public static string NETWORK_PLAYER_PREFAB_NAME = "Network Player";
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Unity.XR.CoreUtils;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField] private Transform head;
+    [SerializeField] private Transform leftHand;
+    [SerializeField] private Transform rightHand;
+
+    private Transform headOrigin;
+    private Transform leftHandOrigin;
+    private Transform rightHandOrigin;
+
+    #endregion
+
+    #region Public Fields
+
+    public PhotonView photonView { get; set; }
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        XROrigin origin = FindObjectOfType<XROrigin>();
+        headOrigin = origin.transform.Find("Camera Offset/Main Camera");
+        leftHandOrigin = origin.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandOrigin = origin.transform.Find("Camera Offset/RightHand Controller");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            head.GetChild(0).gameObject.SetActive(false);
+            leftHand.GetChild(0).gameObject.SetActive(false);
+            rightHand.GetChild(0).gameObject.SetActive(false);
+
+            MapPosition(head, headOrigin);
+            MapPosition(leftHand, leftHandOrigin);
+            MapPosition(rightHand, rightHandOrigin);
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    void MapPosition(Transform target, Transform originTransform)
+    {
+        target.position = originTransform.position;
+        target.rotation = originTransform.rotation;
+    }
+
+    #endregion
+}
