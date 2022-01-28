f57dcfd30c7d114032234e3cf4b86868a6c4618e
Rami <44623786+Rami-J@users.noreply.github.com>
2022-01-27 21:41:01 -0500

    Started player name scripting

---
+++ Assets/Scripts/UI/LauncherSettingsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace EasyMeshVR.UI
+{
+    public class LauncherSettingsMenu : MonoBehaviour
+    {
+        #region Private Fields
+
+        [SerializeField]
+        private TMP_InputField playerNameInputField;
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+    }
+}
