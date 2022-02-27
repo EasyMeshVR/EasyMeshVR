1d2db405bb48871ef1a822078427119e9b1f5b3d
Dylan <dylantilley@knights.ucf.edu>
2022-02-26 20:15:33 -0500

    Resize Radius Rework
    Resize radius of both controller spheres with the respective thumbstick

--- Assets/Scripts/Misc/ResizeRadius.cs
+++ Assets/Scripts/Misc/ResizeRadius.cs
@@ -1,38 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 // Resize sphere collider radius based on midpoint
 public class ResizeRadius : MonoBehaviour
 {
-    [SerializeField] private ControllersMidpoint ControllersMidpointObject;
     [SerializeField] SphereCollider sphereCollider;
 
     [SerializeField] GameObject sphereVisual;
 
+     [SerializeField] public InputAction thumbstick;
 
-    Transform editingSpace;
+     float value;
 
-    void Start()
-    {
-        editingSpace = GameObject.Find("EditingSpace").transform;
-    }
+    //  void Awake()
+    //  {
+    //  }
 
-    // Resize raidus every frame
-    void Update()
-    {
-        Vector3 scale = ControllersMidpointObject.transform.localScale;
+    // public void OnMove(InputValue input)
+    // {
+    //     value = input.Get<Vector2>();
 
-        // Prevents the radius from getting too big relative to the mesh
-        if(.25f * editingSpace.localScale.sqrMagnitude <=  scale.sqrMagnitude)
-            return;
+    //     //moveVec = new Vector3(inputVec.x, 0, inputVec.y);
+    // }
 
-        else
+     void Update()
+     {
+        thumbstick.performed += ctx => Debug.Log("Left Value: " + ctx.ReadValue<Vector2>());
+
+        thumbstick.Enable();  
+        value = thumbstick.ReadValue<Vector2>().x + thumbstick.ReadValue<Vector2>().y;
+        print("value is " + value);
+        // probably need a max to be less than
+        // avoid dividing by 0
+        if(Mathf.Abs(value) > 0.1f)
         {
-            sphereCollider.radius = scale.sqrMagnitude;
-            sphereVisual.transform.localScale = scale;
-            return;
+            sphereCollider.radius += value/100;
+            sphereVisual.transform.localScale += new Vector3(value/100, value/100, value/100);
+        }
+        if(sphereCollider.radius < 0.1f)
+        {
+            sphereCollider.radius = .1f;
+            sphereVisual.transform.localScale = new Vector3(.1f, .1f, .1f);
+            Vector3 spherePos = sphereVisual.transform.position;
+            // add somehting for moving it closer and further also up there
+            //sphereVisual.transform.localPosition = new Vector3(spherePos.x, spherePos.y, .1f);
+
+
         }
 
-    }
+
+
+
+
+     }
+
+     
+
+
+
 }
